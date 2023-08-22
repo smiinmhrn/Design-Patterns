58865e9bf696c37eeef0316e5bd2917a79e0d82e
Samin <samin.mhr2004@gmail.com>
2023-08-22 20:55:00 +0330

    Flyweight Pattern tmp/ex

---
+++ src/Structural/Flyweight/CellContextFactory.java
@@ -0,0 +1,19 @@
+package Structural.Flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class CellContextFactory {
+  private Map<Integer, CellContext> contexts = new HashMap<>();
+
+  public CellContext getContext(String fontFamily, int fontSize, boolean isBold) {
+
+    var hash = Objects.hash(fontFamily, fontSize, isBold);
+
+    if (!contexts.containsKey(hash))
+      contexts.put(hash, new CellContext(fontFamily, fontSize, isBold));
+
+    return contexts.get(hash);
+  }
+}
