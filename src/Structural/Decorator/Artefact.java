c8a4d33ababbaee136d651a7143c16f246e50e4d
Samin <samin.mhr2004@gmail.com>
2023-08-22 20:25:58 +0330

    Decorator Pattern tmp/ex

---
+++ src/Structural/Decorator/Artefact.java
@@ -0,0 +1,14 @@
+package Structural.Decorator;
+
+public class Artefact implements AbstractArtefact {
+    private String name;
+
+    public Artefact(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String render() {
+        return name;
+    }
+}
